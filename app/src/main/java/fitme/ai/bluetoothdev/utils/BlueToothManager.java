aaa71b1544748ee7e6c48acb7495132b21f059e1
DESKTOP-RA5OAFD\gqgz2 <gqgz20041231@qq.com>
2018-05-22 09:58:27 +0800

    add blueToothManager11

--- app/src/main/java/fitme/ai/bluetoothdev/utils/BlueToothManager.java
+++ app/src/main/java/fitme/ai/bluetoothdev/utils/BlueToothManager.java
@@ -1,6 +1,11 @@
 package fitme.ai.bluetoothdev.utils;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.os.Build;
+import android.os.Handler;
+import android.text.TextUtils;
 
 /**
  * Created by hongy on 2018/3/19.
@@ -8,16 +13,24 @@ import android.bluetooth.BluetoothAdapter;
 
 public class BlueToothManager {
 
+    private static Context mContext;
+    private static final int INTERVAL_TIME = 10000;
+    private Handler mHandler = new Handler();
     private static BluetoothAdapter bluetoothAdapter;
     private static BlueToothManager blueToothManager = null;
-    public static BlueToothManager getInstance(){
-        if (blueToothManager==null){
-            blueToothManager = new BlueToothManager();
-            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-            bluetoothAdapter.setName("AYAH_HOME");  //蓝牙改名
-        }
-        return blueToothManager;
+
+    private static class BlueToothHolder{
+        private static BlueToothManager blueToothManager = new BlueToothManager();
     }
+
+    public static BlueToothManager getInstance(Context context){
+        mContext = context.getApplicationContext();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter.setName("AYAH_HOME");  //蓝牙改名
+        return BlueToothHolder.blueToothManager;
+    }
+
+    public BlueToothManager(){}
 
     /**
        打开蓝牙
@@ -41,4 +54,58 @@ public class BlueToothManager {
         }
     }
 
+    /***********
+     * 扫描设备
+     ********/
+    private void scanLeDevice(final boolean enable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            if (enable) {
+                devices.clear();//清空集合
+                // Stops scanning after a pre-defined scan period.
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                            bluetoothAdapter.stopLeScan(mLeScanCallback);
+                        }
+                    }
+                }, INTERVAL_TIME);
+                bluetoothAdapter.startLeScan(mLeScanCallback);
+            } else {
+                try {
+                    bluetoothAdapter.stopLeScan(mLeScanCallback);
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+
+    private void initCallBack(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (device != null) {
+                                if (!TextUtils.isEmpty(device.getName())) {
+                                    // devices.add(device);
+                                    String name = device.getName();
+                                    if (name.contains(BluetoothDeviceAttr.OYGEN_DEVICE_NAME)) {
+                                        if (!devices.contains(device)) {
+                                            devices.add(device);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+            };
+        } else {
+            L.i("设备蓝牙版本过低");
+            return;
+        }
+    }
 }
